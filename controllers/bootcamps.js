af86ddddebd1babeccd25340027f5ff1ce603951
arjunm052 <singh.arjun127@gmail.com>
2020-09-16 08:59:23 +0530

    Finished Auth 1

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,17 +1,14 @@
 const Bootcamp = require("../models/Bootcamp");
 const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
+const geocoder = require("../utils/geocoder");
+const path = require("path");
 
 //@desc    Get all bootcamps
 //@route   GET /api/v1/bootcamps
 //@access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    data: bootcamps,
-  });
+  res.status(200).json(res.advancedResults);
 });
 
 //@desc    Get a single bootcamp by its id
@@ -61,11 +58,12 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
     data: bootcamp,
   });
 });
+
 //@desc    Delete bootcamp
 //@route   DELETE /api/v1/bootcamps/:id
 //@access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
@@ -73,8 +71,85 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
 
+  bootcamp.remove();
+
   res.status(200).json({
     success: true,
     data: {},
   });
+});
+
+//@desc    Get Bootcamps within a radius
+//@route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+//@access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  //Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  //Calculate radius using radians
+  //Divide distance distance by radius of earth
+  //Earth radius is 3963 mil
+  const radius = distance / 3963;
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
+
+//@desc    Upload photo for bootcamp
+//@route   PUT /api/v1/bootcamps/:id/photo
+//@access  Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse("Please Upload a file", 400));
+  }
+
+  const file = req.files.file;
+
+  //Make sure the image is a photo
+  if (!file.mimetype.startsWith("image")) {
+    return next(new ErrorResponse("Please Upload an Image File", 400));
+  }
+
+  //Check file size
+  if (file.size > process.nextTick.MAX_FILE_UPLOAD) {
+    return next(
+      new ErrorResponse(
+        `Please Upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
+        400
+      )
+    );
+  }
+  //Create custom file name
+  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+    if (err) {
+      console.error(err);
+      return next(new ErrorResponse(`Problem with file upload`, 500));
+    }
+
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+    res.status(200).json({
+      success: true,
+      data: file.name,
+    });
+  });
+  console.log(file.name);
 });
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const morgan = require("morgan");
 const colors = require("colors");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -13,19 +16,33 @@ connectDB();
 
 //Load Route Files
 const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
 //Body Parser
 app.use(express.json());
 
+//Cookie Parser
+app.use(cookieParser());
+
 //Dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+//File Upload
+app.use(fileUpload());
+
+// Set static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
 //Mount Routes
 app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
+
 app.use(errorHandler);
 
 //Get a Port Number
